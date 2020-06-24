04d29a1a8ad0c9b0f4725be45a8df355d0b08151
dhruvi618 <dhruvisheth18@gmail.com>
2020-06-24 12:04:50 -0400

    Add necessary node modules

---
+++ frontend/build/precache-manifest.ca922cfd94f80502c211506d89fae780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5e6436c734ea9b0890b70a231b4dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44c249774d662ed708c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c026acc378276e790864",
+    "url": "/static/js/2.6fecacdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fecacdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c249774d662ed708c9",
+    "url": "/static/js/main.ec5153d1.chunk.js"
+  },
+  {
+    "revision": "4dd7b55dee67d280d6f3",
+    "url": "/static/js/runtime-main.130e18cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

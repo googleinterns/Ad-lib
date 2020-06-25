4a2f8319a308ce1352e12120f606130e0c8b2462
dhruvi618 <dhruvisheth18@gmail.com>
2020-06-25 18:47:08 -0400

    add basic form and menu bar components to UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+//import ReactDOM from 'react-dom';
 import './App.css';
-import TextField from '@material-ui/core/TextField';
+import MenuBar from './menu-bar.js';
+import Form from './form.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br></br>
-        <form noValidate autoComplete="off">
-          <TextField id="standard-basic" label="Standard" />
-        </form>
-      </header>
+    <div>
+      <MenuBar />
+      <Form />
     </div>
   );
 }
---
+++ frontend/src/menu-bar.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import HelpIcon from "@material-ui/icons/Help";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Ad-lib
+          </Typography>
+          {
+            <div>
+              <IconButton
+                aria-label="help-icon"
+                aria-controls="help-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <HelpIcon />
+              </IconButton>
+              <Menu
+                id="help-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>About</MenuItem>
+                <MenuItem onClick={handleClose}>FAQ</MenuItem>
+              </Menu>
+            </div>
+          }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}

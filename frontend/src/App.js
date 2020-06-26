6ac755fe3253143805af6e9922c6c65cfe77e007
dhruvi618 <dhruvisheth18@gmail.com>
2020-06-26 18:31:47 -0400

    Add indentation within App.js div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,15 +17,15 @@ function App() {
   return (
     <div>
         <MenuBar />
-      <div className={classes.contentStyle}>
-        <h1>Meet fellow Googlers <em>now</em>!</h1>
-        <h4>Miss bumping into new faces at the office? Want an easy, fun, spontaneous way of meeting 
-            Googlers virtually? Now you can!</h4>
-        <h4>Ad-lib matches you with a fellow Googler in the queue, notifies you through email when 
-            you’ve been matched, and adds an event to your Calendar with a Meet link for you to 
-            join immediately! It also provides a starter question to get the conversation flowing!</h4>
-      </div>
-      <Form />
+        <div className={classes.contentStyle}>
+          <h1>Meet fellow Googlers <em>now</em>!</h1>
+          <h4>Miss bumping into new faces at the office? Want an easy, fun, spontaneous way of meeting 
+              Googlers virtually? Now you can!</h4>
+          <h4>Ad-lib matches you with a fellow Googler in the queue, notifies you through email when 
+              you’ve been matched, and adds an event to your Calendar with a Meet link for you to 
+              join immediately! It also provides a starter question to get the conversation flowing!</h4>
+        </div>
+        <Form />
     </div>
   );
 }

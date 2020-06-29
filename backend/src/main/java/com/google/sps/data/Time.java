470cc67dbabbea4f4c69dd85d2a9d502ac3fcce6
Janet Wang <jzwang8@gmail.com>
2020-06-29 17:50:01 +0000

    Add edge case tests, condense constructors, improve comments

--- backend/src/main/java/com/google/sps/data/Time.java
+++ backend/src/main/java/com/google/sps/data/Time.java
@@ -3,28 +3,27 @@ package com.google.sps.data;
 import java.util.Date;
 import java.util.Calendar;
 
-/** Class to convert Time */
+/** Class used to set reference date/time for testing and convert input timeAvailableUntil to date timestamp */
 public final class Time {
 
-  private static Date date;
+  private Date date;
 
   /** Constructor */
   public Time() {
-    date = new Date();
+    this(new Date());
   }
 
   /** Constructor with manually set date */
-  public Time(Date date) {
+  private Time(Date date) {
     this.date = date;
   }
 
   /** Return today's date with time of hours:minutes in milliseconds since epoch */
-  public static long getTimeMillis(int hours, int minutes) {
-    
+  public long getTimeMillis(int hours, int minutes) {
+    // Validate hours and minutes
     if (hours < 0 || hours >= 24) {
       throw new IllegalArgumentException("Hours can only be 0 through 23 (inclusive).");
     }
-
     if (minutes < 0 || minutes >= 60) {
       throw new IllegalArgumentException("Minutes can only be 0 through 59 (inclusive).");
     }
@@ -32,7 +31,7 @@ public final class Time {
     // Calculate today's date but with hours:minutes time
     // TODO: All times are currently today, wrap around times?
     Calendar c = Calendar.getInstance();
-		c.setTime(date);
+    c.setTime(date);
     c.set(Calendar.HOUR_OF_DAY, hours);
     c.set(Calendar.MINUTE, minutes);
     c.set(Calendar.SECOND, 0);
--- backend/src/test/java/com/google/sps/TimeTest.java
+++ backend/src/test/java/com/google/sps/TimeTest.java
@@ -18,7 +18,7 @@ public final class TimeTest {
   public void setUp() {
     // Set "current" date to  1/1/2020 2:00pm GMT
     Calendar c = Calendar.getInstance();
-    c.set(2020, 0, 1, 14, 0, 0);
+    c.set(/* year= */ 2020, /* month= */ 0, /* date= */ 1, /* hour= */ 14, /* minute= */ 0, /* second= */ 0);
     c.set(Calendar.MILLISECOND, 0);
     Date date = c.getTime();
 
@@ -29,23 +29,43 @@ public final class TimeTest {
   public void inputTimeToMillis() {
     // Set year, month, date and make second and milliseconds 0.
     Calendar c = Calendar.getInstance();
-    c.set(2020, 0, 1);
+    c.set(/* year= */ 2020, /* month= */ 0, /* date= */ 1);
     c.set(Calendar.SECOND, 0);
     c.set(Calendar.MILLISECOND, 0);
     
     // 1/1/2020 2:00pm GMT
     c.set(Calendar.HOUR_OF_DAY, 14);
     c.set(Calendar.MINUTE, 0);
-    Assert.assertEquals(/* expected */c.getTime().getTime(), /* actual */Time.getTimeMillis(14, 00));
+    Assert.assertEquals(/* expected */c.getTime().getTime(), /* actual */time.getTimeMillis(14, 0));
 
     // 1/1/2020 10:30am GMT
     c.set(Calendar.HOUR_OF_DAY, 10);
     c.set(Calendar.MINUTE, 30);
-    Assert.assertEquals(c.getTime().getTime(), Time.getTimeMillis(10, 30));
+    Assert.assertEquals(c.getTime().getTime(), time.getTimeMillis(10, 30));
 
     // 1/1/2020 11:45pm GMT
     c.set(Calendar.HOUR_OF_DAY, 23);
     c.set(Calendar.MINUTE, 45);
-    Assert.assertEquals(c.getTime().getTime(), Time.getTimeMillis(23, 45));
+    Assert.assertEquals(c.getTime().getTime(), time.getTimeMillis(23, 45));
+  }
+
+  @Test (expected = IllegalArgumentException.class) 
+  public void negativeHours() {
+    long timestamp = time.getTimeMillis(-1, 0);
+  }
+
+  @Test (expected = IllegalArgumentException.class) 
+  public void over24Hours() {
+    long timestamp = time.getTimeMillis(24, 0);
+  }
+
+  @Test (expected = IllegalArgumentException.class) 
+  public void negativeMinutes() {
+    long timestamp = time.getTimeMillis(2, -1);
+  }
+
+  @Test (expected = IllegalArgumentException.class) 
+  public void over24Hours() {
+    long timestamp = time.getTimeMillis(2, 60);
   }
 }

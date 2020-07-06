ba51b1737c6d2eeac462967e31c083ac0df980f2
dhruvi618 <dhruvisheth18@gmail.com>
2020-07-06 18:48:11 -0400

    Move .gitignore file from frontend directory to outer directory

---
+++ frontend/build/precache-manifest.218a9e1971816dea892ee025cdc29050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b0893722860b725f186dc85a5234d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "208256032a6e44110c41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "032e5e6e5b717faf4eb6",
+    "url": "/static/js/2.5f0fe39e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5f0fe39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208256032a6e44110c41",
+    "url": "/static/js/main.2f203887.chunk.js"
+  },
+  {
+    "revision": "4dd7b55dee67d280d6f3",
+    "url": "/static/js/runtime-main.130e18cc.js"
+  }
+]);

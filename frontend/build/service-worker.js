be93fb61594a5b5d3e2eb01ca47c5da31f3a38bf
dhruvi618 <dhruvisheth18@gmail.com>
2020-07-06 16:17:29 -0400

    Remove node modules and change node version to 10.21.0 to ensure compatability with GCP

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca922cfd94f80502c211506d89fae780.js"
+  "/precache-manifest.218a9e1971816dea892ee025cdc29050.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

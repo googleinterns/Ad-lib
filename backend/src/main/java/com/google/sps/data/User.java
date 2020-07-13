65e7db6b76cec7ac022c9a2987baeb40e1a86a43
Janet Wang <jzwang8@gmail.com>
2020-07-13 21:18:15 +0000

    Add User and UserDatastore

--- backend/src/main/java/com/google/sps/data/User.java
+++ backend/src/main/java/com/google/sps/data/User.java
@@ -1,6 +1,5 @@
 package com.google.sps.data;
 
-
 /** A user with saved preferences. */
 public final class User {
 
@@ -8,14 +7,12 @@ public final class User {
   private final long id;
   /** Google username (ldap) */
   private final String username;
-  /** Time of submitted form */
-  private final long timestamp;
 
   /** Initialize constructor fields */
-  public Participant(long id, String username, long timestamp) {
+  public User(long id, String username) {
+    // TODO(#32): add User interests
     this.id = id;
     this.username = username;
-    this.timestamp = timestamp;
   }
 
   public long getId() {
--- backend/src/main/java/com/google/sps/datastore/UserDatastore.java
+++ backend/src/main/java/com/google/sps/datastore/UserDatastore.java
@@ -10,10 +10,6 @@ public final class UserDatastore {
   // Datastore Key/Property constants
   private static final String KIND_USER = "User";
   private static final String PROPERTY_USERNAME = "username";
-  private static final String PROPERTY_TIMESTAMP = "timestamp";
-
-  /** Formatter for converting between String and ZonedDateTime */
-  private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
 
   /** Datastore */
   private final DatastoreService datastore;
@@ -28,19 +24,49 @@ public final class UserDatastore {
     // Set properties of entity based on user fields
     Entity userEntity = new Entity(KIND_USER, user.getUsername());
     userEntity.setProperty(PROPERTY_USERNAME, user.getUsername());
-    userEntity.setProperty(PROPERTY_TIMESTAMP, user.getTimestamp());
 
     // Insert entity into datastore
     datastore.put(userEntity);
   }
 
-  /** Return String representation of participants for logging purposes */
+  /** Return User Entity from username, or null if entity is not found */
+  @Nullable
+  private Entity getEntityFromUsername(String username) {
+    Key userKey = KeyFactory.createKey(KIND_USER, username);
+    try {
+      return datastore.get(userKey);
+    } catch (EntityNotFoundException e) {
+      return null;
+    }
+  }
+
+  /** Return User from username, or null if user with username not in datastore */
+  @Nullable
+  public User getUserFromUsername(String username) {
+    return getUserFromEntity(getEntityFromUsername(username));
+  }
+
+  /** Return user object from datastore user entity, or null if entity is null */
+  @Nullable
+  private static User getUserFromEntity(Entity entity) {
+    if (entity == null) {
+      return null;
+    }
+
+    // Get entity properties
+    String username = (String) entity.getProperty(PROPERTY_USERNAME);
+
+    // Create and return new User
+    return new User(id, username);
+  }
+
+  /** Return String representation of users for logging purposes */
   public String toString() {
     StringBuilder sb = new StringBuilder();
     Query query = new Query(KIND_USER);
     PreparedQuery results = datastore.prepare(query);
     for (Entity entity : results.asIterable()) {
-      User user = getParticipantFromEntity(entity);
+      User user = getUserFromEntity(entity);
       sb.append("username=" + user.getUsername());
     }
     return sb.toString();

a37710dbadd32b847521fd645e261e5174b0829c
Jordan Grant <jordangrant46@gmail.com>
2020-07-16 18:33:01 -0400

    Fix

---
+++ backend/src/main/java/com/google/sps/data/Match.java
@@ -0,0 +1,55 @@
+package com.google.sps.data;
+
+/**
+ * A match of two participants. Note: Match includes entire Participants (with unique ID per
+ * submitted form) instead of just usernames because a username may be involved in multiple matches
+ * over time. This issue can be resolved if we delete matches from the datastore after the users are
+ * notified of the match.
+ */
+public final class Match {
+
+  /** Datastore entity ID */
+  private final long id;
+  /** First participant */
+  private final com.google.sps.data.Participant firstParticipant;
+  /** Second participant */
+  private final com.google.sps.data.Participant secondParticipant;
+  /** Duration of meeting */
+  private final int duration;
+  /** Time match found */
+  private final long timestamp;
+
+  /** Constructor */
+  public Match(
+      long id,
+      com.google.sps.data.Participant firstParticipant,
+      com.google.sps.data.Participant secondParticipant,
+      int duration,
+      long timestamp) {
+    this.id = id;
+    this.firstParticipant = firstParticipant;
+    this.secondParticipant = secondParticipant;
+    this.duration = duration;
+    this.timestamp = timestamp;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public com.google.sps.data.Participant getFirstParticipant() {
+    return firstParticipant;
+  }
+
+  public com.google.sps.data.Participant getSecondParticipant() {
+    return secondParticipant;
+  }
+
+  public int getDuration() {
+    return duration;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+}

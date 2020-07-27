5277aac2720352a01b21cccc2749e7ad032434db
dhruvi618 <dhruvisheth18@gmail.com>
2020-07-27 09:10:16 -0400

    Add copyright header to each new component and style changes

--- frontend/src/components/LoadingPage.js
+++ frontend/src/components/LoadingPage.js
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -31,7 +45,7 @@ export default function LoadingPage() {
               learn about <a href="https://guidetoallyship.com/">Allyship</a>,
               or even make a smoothie! We will send you an <b>email </b>
               and <b>Calendar invite</b> as soon as we find you a match!</p>
-          <Button variant="contained" color="primary">Exit Queue</Button>   
+          <Button variant="contained" color="primary">Exit Queue</Button>
         </CardContent>
       </Card>
     </div>

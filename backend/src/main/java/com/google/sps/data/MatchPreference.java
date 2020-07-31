029235aa602d78eb97d80c0e79683b06a6b73b1e
Janet Wang <jzwang8@gmail.com>
2020-07-31 22:04:56 +0000

    Fix merge conflict

--- backend/src/main/java/com/google/sps/data/MatchPreference.java
+++ backend/src/main/java/com/google/sps/data/MatchPreference.java
@@ -42,19 +42,6 @@ public enum MatchPreference {
     throw new IllegalStateException("Unknown enum int value.");
   }
 
-  public static MatchPreference forStringValue(String value) {
-    switch (value.toLowerCase()) {
-      case "different":
-        return MatchPreference.DIFFERENT;
-      case "any":
-        return MatchPreference.ANY;
-      case "similar":
-        return MatchPreference.SIMILAR;
-      default:
-        throw new IllegalStateException("Unknown enum String value.");
-    }
-  }
-
   /** Parse match preference string to MatchPreference */
   public static MatchPreference forStringValue(String value) {
     String lowerCasePreference = value.toLowerCase();
